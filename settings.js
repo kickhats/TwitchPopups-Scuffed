dabb9b812140a77ba1e219d194707f6771b45d95
Ciaran <39930930+kickhats@users.noreply.github.com>
2022-04-23 12:46:15 +0100

    init commit

---
+++ settings.js
@@ -0,0 +1,22 @@
+// Change kickhats to your Twitch channel
+const twitchChannel = 'kickhats';
+
+// Your alert background. Default is a vibrant green
+ const alertBg = '#0080c9';
+
+// Spotlight background colour. Default is a deep, rich "gold"
+const spotlightBg = '#bdb300';
+
+// The emoji that surrounds the spotlight messages. Default is the Pride Flag emoji.
+const spotlightEmoji = '🏳️‍🌈';
+
+// Allow twitch emotes, can be true or false.
+const twitchEmotes = true;
+
+// Play a sound when the "!alert" command is used
+const playAlertSound = false;
+
+// The location of the alert sound that will be used if "playAlertSound" is true, path is relative to "twitchpopups.html"
+const alertSoundFile = "assets/sounds/alert.mp3";
+
+// Follow the instructions in README.md
